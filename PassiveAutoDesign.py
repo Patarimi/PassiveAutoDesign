4f90a08a7d86ace2f2e276c7ea7f09a9d1a1f538
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-05-20 13:09:47 +0200

    balun fonctions addition

--- PassiveAutoDesign.py
+++ PassiveAutoDesign.py
@@ -60,7 +60,45 @@ def coupleur_print(res, bounds):
     print(f'best point  :\t{sol[0]:.2g}\t{res.x[1]:.2g}\t{sol[2]:.3g}\t{sol[3]:.2g}')
     print(f'upper bound :\t{(bds[0])[1]:.2g}\t{(bounds[1])[1]:.2g}\t{(bds[2])[1]:.3g}\t{(bds[3])[1]:.2g}')
 
+#Impendance Transformer Specific Function
+def balun_cost(sol, k, zl_targ, zs_targ, f_targ):
+    """
+        return the cost (standard deviation)
+        between the proposed solution and the targeted specifications
+    """
+    sol[1] = np.round(sol[1])
+    sol[5] = np.round(sol[5])
+    l_source = l_geo(sol[0], sol[3], sol[1], sol[2])
+    l_load = l_geo(sol[4], sol[7], sol[5], sol[6])
+    alpha = (1-k**2)/k**2
+    n_turn = k*np.sqrt(l_source/l_load)
+    z_load = 1j*l_source*(k**2)*2*np.pi*f_targ
+    zs_r = alpha*z_load + z_load*(n_turn**2)*zl_targ/(z_load+zl_targ*(n_turn**2))
+    zl_r = ((np.conj(zs_targ)+alpha*z_load)*z_load/(np.conj(zs_targ)+z_load+alpha*z_load))/n_turn**2
+    return std_dev(np.array([zs_r, zl_r]), np.array([zs_targ, zl_targ]))
 
+def balun_design(f_targ, zl_targ, zs_targ, bounds, k):
+    """
+        design an impedance transformer with the targeted specifications (f_targ, zl_targ, zs_targ)
+        return an optimization results (res)
+    """
+    res = dual_annealing(balun_cost, bounds, maxiter=1000, args=(k, zl_targ, zs_targ, f_targ))
+    return res
+
+def balun_print(res, bounds):
+    """
+        print a summary of the solution (res)
+        with a comparison to the boundaries
+    """
+    sol = res.x*1e6
+    bds = np.array(bounds)*1e6
+    print(f'Solution funds with remaining error of: {res.fun:.2e}')
+    print('Termination message of algorithm: '+str(res.message))
+    print(f'\t\tW (µm)\tn\tdi (µm)\tG (µm)')
+    print(f'lower bound :\t{(bds[0])[0]:.2g}\t{(bounds[1])[0]:.2g}\t{(bds[2])[0]:.3g}\t{(bds[3])[0]:.2g}')
+    print(f'primary dim.:\t{sol[0]:.2g}\t{res.x[1]:.2g}\t{sol[2]:.3g}\t{sol[3]:.2g}')
+    print(f'secondary dim.:\t{sol[4]:.2g}\t{res.x[5]:.2g}\t{sol[6]:.3g}\t{sol[7]:.2g}')
+    print(f'upper bound :\t{(bds[0])[1]:.2g}\t{(bounds[1])[1]:.2g}\t{(bds[2])[1]:.3g}\t{(bds[3])[1]:.2g}')
 
 #General Purpose Function
 def l_geo(width, gap, n_turn, inner_diam):
--- coupleursizing.py
+++ coupleursizing.py
@@ -5,6 +5,7 @@ Created on Thu Apr 25 15:04:53 2019
 @author: mpoterea
 """
 
+import numpy as np
 import PassiveAutoDesign as pad
 
 #design coupleur hybride
@@ -18,3 +19,24 @@ X_MIN = (2e-6, 1, 50e-6, 2.1e-6)
 RES = pad.coupleur_design(5e9, 50.0, list(zip(X_MIN, X_MAX)), 1.35e-6, 4.3, 0.9)
 pad.coupleur_print(RES, list(zip(X_MIN, X_MAX)))
 
+#design transformateur d'impedances
+ZS_TARG = 20+1j*40
+ZL_TARG = 50 + 1j*0
+K_COEFF = 0.9
+ALPHA = (1-K_COEFF**2)/K_COEFF**2
+F_TARG = 50e9
+Q_S = np.imag(ZS_TARG)/np.real(ZS_TARG)
+Q_L = np.imag(ZL_TARG)/np.real(ZL_TARG)
+A_FACT = (2*ALPHA*Q_S+Q_S+Q_L)/(2*(ALPHA+1))
+B_FACT = np.sqrt((2*ALPHA*Q_S+Q_S+Q_L)**2-4*(ALPHA**2+ALPHA)*(1+Q_S**2))/(2*(ALPHA+1))
+Z_T = np.array([A_FACT+B_FACT, A_FACT-B_FACT])
+
+LS = np.real(ZS_TARG)*Z_T/((1-K_COEFF**2)*(2*np.pi*F_TARG))
+LL = np.real(ZL_TARG)*Z_T*(1+Q_L**2)/(ALPHA*(1+(Q_S-Z_T)**2)*(2*np.pi*F_TARG))
+
+BOUNDS = list(zip(X_MIN+X_MIN, X_MAX+X_MAX))
+RES = pad.balun_design(F_TARG, ZL_TARG, ZS_TARG, BOUNDS, K_COEFF)
+pad.balun_print(RES, BOUNDS)
+
+LS_SYNTH = pad.l_geo(RES.x[0], RES.x[3], RES.x[1], RES.x[2])
+LL_SYNTH = pad.l_geo(RES.x[4], RES.x[7], RES.x[5], RES.x[6])
