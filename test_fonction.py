8280f44ae4194438f6f4810e70e4dfbcaa2bb5e7
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-06-24 16:07:05 +0200

    [Fix] Various modification following c5e61cc

--- test_fonction.py
+++ test_fonction.py
@@ -13,6 +13,7 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import PassiveAutoDesign as pad
 import substrate as sub
+import ngspice_warper as ng
 
 OUTPUT = list()   #contains list of tuples (cost, param sweep for plotting)
 # importation of reference data and cost calculation
@@ -23,21 +24,29 @@ with open('./test_set/coupleur_data.csv', newline='') as data_file:
             if row[0]=='with':
                 BEOL = sub.Substrate(row[1])
             else:
-                x = np.array((float(row[0]), float(row[1]), float(row[2]), float(row[3])))
+                x = {'width':float(row[0]), 'n_turn':float(row[1]), 'di':float(row[2]), 'gap':float(row[3]), 'height':BEOL.sub[0].height}
                 k = float(row[4])
                 f = float(row[5])
                 z = complex(float(row[6]), float(row[7]))
                 # calculation of deviation between calculation and validated values
-                CPL = pad.Coupler(BEOL, f, z)
-                Cost = CPL.cost(x)
+                CPL = pad.Coupler(BEOL, f, z, k)
+                CPL.transfo.set_primary(x)
+                CPL.transfo.set_secondary(x)
+                b_model = bytes(CPL.transfo.generate_spice_model(k), encoding='UTF-8')
+                b_simulation = bytes(ng.generate_ac_simulation(f, f, 1), encoding='UTF-8')
+                with open('./cache/model_coupler.cir', 'w') as file:
+                    file.write(CPL.transfo.generate_spice_model(k))
+                S = ng.get_results(b_model+b_simulation)
+                Cost = np.abs(S[0]-(50-z)/(z+50))
                 # output creation Cost and sweept variables
-                OUTPUT.append((Cost, x[2], x[1]))
+                OUTPUT.append((Cost, x['di'], x['n_turn']))
         except ValueError:
             #importation error gestion
             if row[0]!='#W':
                 print('line skip :'+str(row))
 COST_RES = np.array(OUTPUT)
 
+plt.figure()
 plt.plot(COST_RES[0:5, 1]*1e6, 100*COST_RES[0:5, 0], 'rx')
 plt.plot(COST_RES[5:9, 1]*1e6, 100*COST_RES[5:9, 0], 'b+')
 plt.legend(['n=2', 'n=1'])
@@ -46,7 +55,7 @@ plt.xlabel("Inner Diameter (µm)")
 plt.ylim(bottom=0)
 plt.xlim(left=0)
 plt.grid(True)
-TMP = 100*COST_RES[:,0]
+TMP = list(100*COST_RES[:,0])
 print(f'Typical Error\nmin:\t{np.min(TMP):2.1f}\nmed:\t{np.median(TMP):2.1f}\nmax:\t{np.max(TMP):2.1f}')
 
 # %% drawing of the cost function versus di and W
