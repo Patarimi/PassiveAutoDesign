fd6b392cf2e81389d1c469adff49e4f7e87a2917
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-07-12 09:52:21 +0200

    old ngspice test move to separate test file

--- ngspice_warper.py
+++ ngspice_warper.py
@@ -72,36 +72,3 @@ def get_results(spice_bytes, _dump_results=False):
                    data[3],
                    data[4]))
     return _s
-if __name__ == '__main__':
-    #test fonctions
-    REF_MODEL = 'Hybrid Coupler\n\n\
-VIN\t\t3\t0\tDC\t0\tAC\t1\n\
-RIN\t\t3\tIN\t50\nROUT\tOUT\t0\t50\n\
-RCPL\tCPL\t0\t50\n\
-RISO\tISO\t0\t50\n\nL1\t\tIN\t1\t1.000e-09\n\
-R1\t\t1\tOUT\t5.000e-01\n\
-L2\t\tCPL\t2\t1.000e-09\n\
-R2\t\t2\tISO\t5.000e-01\nK\t\t\
-L1\tL2\t0.9\n\
-CG1\t\tIN\t0\t2.500e-16\n\
-CG2\t\tOUT\t0\t2.500e-16\n\
-CG3\t\tISO\t0\t2.500e-16\n\
-CG4\t\tCPL\t0\t2.500e-16\n\
-CM1\t\tIN\tCPL\t5.000e-16\n\
-CM2\t\tISO\tOUT\t5.000e-16\n\n'
-    S_CTRL = generate_ac_simulation(1e9, 1e9, 1)
-    REF_CTRL = '.AC LIN\t1\t1.000e+09\t1.000e+09\n\
-.PRINT AC V(IN) I(VIN) V(OUT) V(CPL) V(ISO)\n\n\
-.OPTION ELTOL=1e-12\n.END\n'
-    if S_CTRL != REF_CTRL:
-        raise ValueError
-    S = get_results(bytes(REF_MODEL+S_CTRL, encoding='UTF-8'))
-    IRL = -10*np.log10(np.abs(S[0]))
-    if IRL < 12:
-        raise ValueError(f'IRL = {IRL}')
-    IHSR = -10*np.log10(np.abs(S[1]+1j*S[2]))
-    if IHSR < 3:
-        raise ValueError(f'ihsr = {IHSR}')
-    N_STEP = 54
-    S_CTRL = generate_ac_simulation(1e9, 20e9, N_STEP)
-    S = get_results(bytes(REF_MODEL+S_CTRL, encoding='UTF-8'), True)
