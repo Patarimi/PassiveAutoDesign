f84aad12d1718921ad4a440da0fafd1d738c7644
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-07-15 13:22:20 +0200

    [test-fix] avoid possible conflict between test substrate and test apssive_component. they were both write and loading the same tech.yml file. They now use different files.

--- tests/test_passive_component.py
+++ tests/test_passive_component.py
@@ -10,6 +10,6 @@ import substrate as sub
 
 class TestCoupler():
     def test_design(self):
-        SUB = sub.Substrate('tests/tech.yml')
+        SUB = sub.Substrate('tests/test_passive_component_test.yml')
         CPL = pac.Coupler(SUB)
         assert CPL.f_c == 1e9
