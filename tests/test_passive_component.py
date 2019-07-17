5540f0b9416d567ff2c67d48d71c9a56970804cf
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-07-17 11:43:42 +0200

    typo fix

--- tests/test_passive_component.py
+++ tests/test_passive_component.py
@@ -10,6 +10,6 @@ import design.substrate as sub
 
 class TestCoupler():
     def test_design(self):
-        SUB = sub.Substrate('tests\passive_component_tech.yml')
+        SUB = sub.Substrate('tests/passive_component_tech.yml')
         CPL = pac.Coupler(SUB)
         assert CPL.f_c == 1e9
