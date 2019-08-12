8ae767966b711e59dfd2377eddba875ad5a9a043
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-08-12 14:35:27 +0200

    renaming for snake_case conformation

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -8,32 +8,38 @@ import pytest
 import design.structure as st
 from design.substrate import COPPER, D5880
 
-def test_SIW():
-    WR = st.SIW(COPPER, D5880, 2.4e-3)
+def test_siw():
+    """
+    unity test for SIW object
+    """
+    wr1 = st.SIW(COPPER, D5880, 2.4e-3)
 #width not set, pphc should return an error
     with pytest.raises(ValueError):
-        WR.calc_pphc(29e9, 35e5)
-    WR.set_width(7.1e-3)
-    assert round(WR.f_c) == 14233805208
-    WR.set_fc(17e9)
-    assert round(1000*WR.width,2) == 5.94
-    assert round(WR.calc_a_d(20e9),4) == 0.0461
-    assert round(WR.calc_a_c(20e9),6) == 0.004997
-    assert round(WR.calc_ksr(20e9),6) == 1
-    WR.diel.rougthness = 0
+        wr1.calc_pphc(29e9, 35e5)
+    wr1.set_width(7.1e-3)
+    assert round(wr1.f_c) == 14233805208
+    wr1.set_fc(17e9)
+    assert round(1000*wr1.width, 2) == 5.94
+    assert round(wr1.calc_a_d(20e9), 4) == 0.0461
+    assert round(wr1.calc_a_c(20e9), 6) == 0.004997
+    assert round(wr1.calc_ksr(20e9), 6) == 1
+    wr1.diel.rougthness = 0
     with pytest.raises(ValueError):
-        WR.calc_ksr(20e9)
-    WR.diel.rougthness = 0.0009
-    assert round(WR.calc_pphc(29e9, 35e5)) == 139370
-    WR.print_info()
-    assert round(WR.get_sparam(20e9, 10e-3), 2) == (-0.99+0.13j)
+        wr1.calc_ksr(20e9)
+    wr1.diel.rougthness = 0.0009
+    assert round(wr1.calc_pphc(29e9, 35e5)) == 139370
+    wr1.print_info()
+    assert round(wr1.get_sparam(20e9, 10e-3), 2) == (-0.99+0.13j)
 
-def test_AF_SIW():
+def test_af_siw():
+    """
+    unity test for AF-SIW object
+    """
     with pytest.raises(ValueError):
-        WR = st.AF_SIW(COPPER, D5880, 2.4e-3, 0)
-    WR = st.AF_SIW(COPPER, D5880, 2.4e-3, 0.2e-3)
-    WR.set_fc(17e9)
-    assert round(1000*WR.width) == 6.0
-    assert WR.calc_a_d(15e9) == 0
-    assert round(WR.calc_ksr(20e9),6) == 1
-    WR.print_info()
+        af1 = st.AF_SIW(COPPER, D5880, 2.4e-3, 0)
+    af1 = st.AF_SIW(COPPER, D5880, 2.4e-3, 0.2e-3)
+    af1.set_fc(17e9)
+    assert round(1000*af1.width) == 6.0
+    assert af1.calc_a_d(15e9) == 0
+    assert round(af1.calc_ksr(20e9), 6) == 1
+    af1.print_info()
