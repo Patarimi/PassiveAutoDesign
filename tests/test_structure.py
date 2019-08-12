3c0ec11e4c6820a88be39753f21126f63770200b
Manuel Potereau <mpoterea@u-bordeaux.fr>
2019-08-12 14:43:32 +0200

    unity test for transformer

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -43,3 +43,33 @@ def test_af_siw():
     assert af1.calc_a_d(15e9) == 0
     assert round(af1.calc_ksr(20e9), 6) == 1
     af1.print_info()
+
+def test_transformer():
+    """
+    unity test for tranformer object
+    """
+    geo = {'di':20,
+           'n_turn':2,
+           'width':1e-3,
+           'gap':2e-3,
+           'height':1e-6}
+    transfo = st.Transformer(geo, geo)
+    transfo.set_primary(geo)
+    transfo.set_secondary(geo)
+    assert transfo.generate_spice_model(0.99) == 'Transformer Model\n\n\
+VIN\t\t3\t0\tDC\t0\tAC\t1\n\
+RIN\t\t3\tIN\t50\n\
+ROUT\tOUT\t0\t50\n\
+RCPL\tCPL\t0\t50\n\
+RISO\tISO\t0\t50\n\n\
+L1\t\tIN\t1\t226079425.7p\n\
+R1\t\t1\tOUT\t2256609.9m\n\
+L2\t\tCPL\t2\t226079425.7p\n\
+R2\t\t2\tISO\t2256609.9m\n\
+K\t\tL1\tL2\t0.99\n\
+CG1\t\tIN\t0\t0.0f\n\
+CG2\t\tOUT\t0\t0.0f\n\
+CG3\t\tISO\t0\t0.0f\n\
+CG4\t\tCPL\t0\t0.0f\n\
+CM1\t\tIN\tCPL\t500.4f\n\
+CM2\t\tISO\tOUT\t500.4f\n\n'
