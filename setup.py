b1d68a5b5b5a29cc81c658135c9e27406c74a965
mpoterea <mpoterea@u-bordeaux.fr>
2019-11-13 11:32:35 +0100

    next level ! :D

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="passive-auto-design",
-    version="0.0.4",
+    version="0.0.5",
     author="Patarimi",
     author_email="mpqqch@gmail.com",
     description="Tools for fast prototyping of radio-frequence passive components",
