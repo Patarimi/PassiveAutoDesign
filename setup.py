acdb5cc03dec51b640b61c6b0e88c4db88d58e51
mpoterea <mpoterea@u-bordeaux.fr>
2019-11-18 14:09:06 +0100

    faster balun design design of balun with finite quality factor fixing s-parameters simulation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="passive-auto-design",
-    version="0.0.5",
+    version="0.0.6",
     author="Patarimi",
     author_email="mpqqch@gmail.com",
     description="Tools for fast prototyping of radio-frequence passive components",
