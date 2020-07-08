6892419555264450640d008cbdd3c945f12dbf20
Patarimi <mpqqch@gmail.com>
2020-07-08 09:47:12 +0200

    Update test_special.py

--- tests/test_special.py
+++ tests/test_special.py
@@ -3,5 +3,5 @@ import passive_auto_design.special as sp
 
 def test_special():
     with pytest.raises(ValueError):
-        assert sp.std_dev(20+1j*40, 20+1j*40, 50)
+        assert sp.std_dev((20+1j*40, 20+1j*40), 50)
     assert sp.qual_f(100 - 1j*300) == -3.0
