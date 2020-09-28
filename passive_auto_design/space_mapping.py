99d3ee6273e5c318f41b0b1be101a0fef8535886
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-09-28 09:04:47 +0200

    Suppress Class
    Remove class, property and metaclass usage.
    The structure is closer to optimize function in scipy.

--- passive_auto_design/space_mapping.py
+++ passive_auto_design/space_mapping.py
@@ -1,65 +1,53 @@
-import abc
+"""
+    Implementation of an aggressive space mapping algorithm for RF-design
+"""
 from scipy.optimize import minimize
 
-class SpaceMap(metaclass=abc.ABCMeta):
-    def __init__(self):
-        self.__par = {}
-        self.__dim = {}
-        self.__goal = {}
-    
-    @property
-    def par(self):
-        return self.__par
-    @par.setter
-    def par(self, key_val):
-        self.__par.update(key_val)
-        
-    @property
-    def dim(self):
-        return self.__dim
-    @dim.setter
-    def dim(self, key_val):
-        self.__dim.update(key_val)
-    
-    @property
-    def goal(self):
-        return self.__goal
-    @goal.setter
-    def goal(self, key_val):
-        self.__goal.update(key_val)
+def space_map(coarse_model, dim0, fine_model, par0, goal, maxiter=5):
+    dim = dim0
+    par = par0
+    fine_mod = goal
+    for i in range(maxiter):
+        #evaluate exact value using fine model
+        fine_mod = fine_model(dim)
+        #alter coarse model paramaters to better match fine model results
+        res = minimize(__refresh_coarse, __tolist(par), method='L-BFGS-B',
+                       args=(par0.keys(), dim, coarse_model, fine_mod))
+        par = __todict(par0.keys(), res.x)
+        #find best solution according to coarse model
+        res = minimize(__cost_coarse, __tolist(dim), method='Powell',
+                       args=(dim0.keys(), par, coarse_model, goal))
+        dim = __todict(dim0.keys(), res.x)
+    return (dim, par, fine_mod)
 
-    @abc.abstractmethod
-    def coarse_model(self):
-        raise NotImplementedError
-    
-    @abc.abstractmethod
-    def fine_model(self):
-        raise NotImplementedError
+def __cost_coarse(dim_values, dim_keys, par, coarse_model, goal):
+    _dim = __todict(dim_keys, dim_values)
+    coarse_mod = coarse_model(_dim, par)
+    return cost_calc(coarse_mod, goal)
 
-    def solve(self, maxiter=5):
-        for i in range(maxiter):
-            #find best solution according to coarse model
-            res = minimize(self.__cost_coarse, list(self.dim.values()))
-            self.dim = dict(zip(self.dim, res.x))
-            #evaluate exact value using fine model
-            fine_mod = self.fine_model()
-            #alter coarse model paramaters to better match fine model results
-            res = minimize(self.__refresh_coarse, list(self.par.values()), args=(fine_mod))
-            self.par = dict(zip(self.par, res.x))
-        return self.dim
-    
-    def __cost_coarse(self, d):
-        dim = dict(zip(self.dim, d))
-        coarse_mod = self.coarse_model(dim, self.par)
-        err = 0
-        for key, value in coarse_mod.items():
-            err += abs((self.goal[key]-value)/(self.goal[key]+value))
-        return err
-    
-    def __refresh_coarse(self, p, fine_mod):
-        par = dict(zip(self.par, p))
-        coarse_mod = self.coarse_model(self.dim, par)
-        err = 0
-        for key, value in coarse_mod.items():
-            err += abs((fine_mod[key]-value)/(self.goal[key]+value))
-        return err
+def __refresh_coarse(par_values, par_keys, dim, coarse_model, fine_mod):
+    _par = __todict(par_keys, par_values)
+    coarse_mod = coarse_model(dim, _par)
+    return cost_calc(coarse_mod, fine_mod)
+
+def cost_calc(perf_list, goal_list, weight_list=None):
+    if weight_list is None:
+        weight_list = [1]*len(goal_list.values())
+    cost = 0
+    for itern, keys in enumerate(goal_list.keys()):
+        perf = perf_list[keys]
+        goal = goal_list[keys]
+        if type(goal) is tuple:
+            err_min = max((perf-goal[0])/(goal[0] if goal[0]!=0 else 1), 0)
+            err_max = max((goal[1]-perf)/(goal[1] if goal[1]!=0 else 1), 0)
+            err = err_min + err_max
+        else:
+            err = (perf-goal)/(goal if goal!=0 else 1)
+        cost += weight_list[itern]*err**2
+    return cost
+
+def __todict(keys, values):
+    return dict(zip(keys, values))
+
+def __tolist(dicts):
+    return list(dicts.values())
