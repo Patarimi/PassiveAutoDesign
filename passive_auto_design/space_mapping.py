6e2957b144e18b5e154909eded1642c1d8b1d504
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-09-28 09:25:22 +0200

    lru_cache addition
    adding lru cache to increase execution speed.
    use tuple instead of list (hashable)

--- passive_auto_design/space_mapping.py
+++ passive_auto_design/space_mapping.py
@@ -2,6 +2,7 @@
     Implementation of an aggressive space mapping algorithm for RF-design
 """
 from scipy.optimize import minimize
+import functools
 
 def space_map(coarse_model, dim0, fine_model, par0, goal, maxiter=5):
     dim = dim0
@@ -11,11 +12,11 @@ def space_map(coarse_model, dim0, fine_model, par0, goal, maxiter=5):
         #evaluate exact value using fine model
         fine_mod = fine_model(dim)
         #alter coarse model paramaters to better match fine model results
-        res = minimize(__refresh_coarse, __tolist(par), method='L-BFGS-B',
+        res = minimize(__refresh_coarse, __totuple(par), method='L-BFGS-B',
                        args=(par0.keys(), dim, coarse_model, fine_mod))
         par = __todict(par0.keys(), res.x)
         #find best solution according to coarse model
-        res = minimize(__cost_coarse, __tolist(dim), method='Powell',
+        res = minimize(__cost_coarse, __totuple(dim), method='Powell',
                        args=(dim0.keys(), par, coarse_model, goal))
         dim = __todict(dim0.keys(), res.x)
     return (dim, par, fine_mod)
@@ -23,20 +24,21 @@ def space_map(coarse_model, dim0, fine_model, par0, goal, maxiter=5):
 def __cost_coarse(dim_values, dim_keys, par, coarse_model, goal):
     _dim = __todict(dim_keys, dim_values)
     coarse_mod = coarse_model(_dim, par)
-    return cost_calc(coarse_mod, goal)
+    return cost_calc(__totuple(coarse_mod), __totuple(goal))
 
 def __refresh_coarse(par_values, par_keys, dim, coarse_model, fine_mod):
     _par = __todict(par_keys, par_values)
     coarse_mod = coarse_model(dim, _par)
-    return cost_calc(coarse_mod, fine_mod)
+    return cost_calc(__totuple(coarse_mod), __totuple(fine_mod))
 
+@functools.lru_cache()
 def cost_calc(perf_list, goal_list, weight_list=None):
     if weight_list is None:
-        weight_list = [1]*len(goal_list.values())
+        weight_list = [1]*len(goal_list)
     cost = 0
-    for itern, keys in enumerate(goal_list.keys()):
-        perf = perf_list[keys]
-        goal = goal_list[keys]
+    for itern, goal in enumerate(goal_list):
+        perf = perf_list[itern]
+        goal = goal_list[itern]
         if type(goal) is tuple:
             err_min = max((perf-goal[0])/(goal[0] if goal[0]!=0 else 1), 0)
             err_max = max((goal[1]-perf)/(goal[1] if goal[1]!=0 else 1), 0)
@@ -49,5 +51,5 @@ def cost_calc(perf_list, goal_list, weight_list=None):
 def __todict(keys, values):
     return dict(zip(keys, values))
 
-def __tolist(dicts):
-    return list(dicts.values())
+def __totuple(dicts):
+    return tuple(dicts.values())
