404e4013da0ae80fc4172729182cdd16240c77ce
Patarimi <38954040+Patarimi@users.noreply.github.com>
2021-11-07 16:01:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
 
 setuptools.setup(
     name="passive-auto-design",
-    version="0.1.6",
+    version="0.1.7",
     author="Patarimi",
     author_email="mpqqch@gmail.com",
     description="Tools for fast prototyping of radio-frequence passive components",
