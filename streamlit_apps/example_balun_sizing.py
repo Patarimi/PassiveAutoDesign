144db33fc25bc1dd82f5bccd74bb7532d02c846e
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-01-24 13:38:14 +0100

    small re-design of the interface

--- streamlit_apps/example_balun_sizing.py
+++ streamlit_apps/example_balun_sizing.py
@@ -7,22 +7,27 @@ MODEL_MAP_PATH = "./tests/default.map"
 BALUN_TST = bln.Balun(modelmapfile=MODEL_MAP_PATH)
 
 # Design inputs
-BALUN_TST.f_c = st.number_input("f_c (Hz)", value=18.0e9)
-BALUN_TST.z_src = st.number_input("Z_src (Real)", value=50.0) + 1j * st.number_input(
-    "Z_src (Imag)", value=-0.1
-)
-BALUN_TST.z_ld = st.number_input("Z_load (Real)", value=2.0) + 1j * st.number_input(
-    "Z_load (Imag)", value=-11.0
-)
+with st.form(key="design_input"):
+    BALUN_TST.f_c = st.number_input("f_c (Hz)", value=18.0e9)
+    col1, col2 = st.columns(2)
+    BALUN_TST.z_src = col1.number_input("Z_src (Real)", value=50.0) + 1j * col2.number_input(
+        "Z_src (Imag)", value=-0.1
+    )
+    BALUN_TST.z_ld = col1.number_input("Z_load (Real)", value=2.0) + 1j * col2.number_input(
+        "Z_load (Imag)", value=-11.0
+    )
 
 # force symmetrical balun by altering load
-if st.checkbox("enforce symmetrical balun"):
-    BALUN_TST.enforce_symmetrical("load")
-st.text(BALUN_TST.z_ld)
-st.text(BALUN_TST.z_src)
-RES2 = BALUN_TST.design()
-st.text(BALUN_TST.print(RES2))
+    if st.checkbox("enforce symmetrical balun"):
+        BALUN_TST.enforce_symmetrical("load")
+    submitted = st.form_submit_button()
 
-# Inductance of the primary and secondary inductors
-LS_SYNTH = BALUN_TST.transfo.model["ls"]
-LL_SYNTH = BALUN_TST.transfo.model["lp"]
+    if submitted:
+        st.text(BALUN_TST.z_ld)
+        st.text(BALUN_TST.z_src)
+        RES2 = BALUN_TST.design()
+        st.text(BALUN_TST.print(RES2))
+
+        # Inductance of the primary and secondary inductors
+        BALUN_TST.transfo.model["ls"]
+        BALUN_TST.transfo.model["lp"]
