5aae85712dbe0c3e2f6d695b57ee33993756deb7
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-01-24 13:48:00 +0100

    restore test_integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,11 +2,11 @@ import glob
 
 
 def test_examples():
-    example = glob.glob("./docs/examples/*.py")
+    example = glob.glob("./streamlit_apps/*.py")
     i = 0
     for file in example:
         print(file)
         with open(file, "r") as f:
             exec(f.read(), globals())
         i += 1
-    assert i == 5
+    assert i == 6
