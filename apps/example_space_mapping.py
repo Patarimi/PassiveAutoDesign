b50495410e271beeed83f2e1b3e419d2dfc42f6e
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-01-31 15:15:50 +0100

    Adding the space mapping algorithm example

--- apps/example_space_mapping.py
+++ apps/example_space_mapping.py
@@ -2,53 +2,48 @@
     Space_mapping module implementation example
 
 """
-import skrf as rf
+import streamlit as st
+from hydralit import HydraHeadApp
 from passive_auto_design.space_mapping import space_map
 from passive_auto_design.special import eps0
 import passive_auto_design.components.lumped_element as lp
 
-# definition of the goal (or target)
-goal = {"C": 800e-12, "R": (0, 1)}
 
-# list of the parameters use by the component model
-par0 = {"rho/h": 1e-6, "eps/d": 3}
+class SpaceMap(HydraHeadApp):
+    def run(self):
+        st.header("Space mapping example : Optimisation of a capacitor parasitic resistor")
+        st.write("Definition of the goal (or target)")
+        goal = {"C": 800e-12, "R": (0, 1)}
+        st.experimental_show(goal)
+        st.write("List of the parameters use by the component model.")
+        par0 = {"rho/h": 1e-6, "eps/d": 3}
+        st.experimental_show(par0)
+        st.write("List of the dimensions of the component.")
+        dim0 = {"l": 1e-3, "w": 1e-6}
+        st.experimental_show(dim0)
 
-# list of the dimensions of the component
-dim0 = {"l": 1e-3, "w": 1e-6}
+        st.write("Definition of the coarse model.")
+        with st.echo():
+            def coarse_model(dim, par):
+                cap = lp.Capacitor(area=dim["w"] * dim["l"], dist=1, eps_r=par["eps/d"])
+                res = lp.Resistor(section=dim["w"], length=dim["l"], rho=par["rho/h"])
+                achieved = {
+                    "C": cap.par["cap"],
+                    "R": res.par["res"],
+                }
+                return achieved # should contain all the keys of the goal
 
+        st.write("Definition of the fine model.")
+        with st.echo():
+            def fine_model(dim):
+                achieved = {
+                    "C": eps0 * 5 * dim["w"] * dim["l"],
+                    "R": 0.2 * dim["l"] / dim["w"],
+                }
+                return achieved # should contain all the keys of the goal
 
-def coarse_model(dim, par):
-    """
-    Definition of the coarse model.
+        with st.echo():
+            dim_f, par_f, goal_f = space_map(coarse_model, dim0, fine_model, par0, goal)
 
-    Returns
-        dict
-            should contain all the keys of the goal
-    """
-
-    cap = lp.Capacitor(area=dim["w"] * dim["l"], dist=1, eps_r=par["eps/d"])
-    res = lp.Resistor(section=dim["w"], length=dim["l"], rho=par["rho/h"])
-    achieved = {
-        "C": cap.par["cap"],
-        "R": res.par["res"],
-    }
-    return achieved
-
-
-def fine_model(dim):
-    """
-    Definition of the fine model.
-
-    Returns
-        dict
-            should contain all the keys of the goal
-    """
-
-    achieved = {
-        "C": eps0 * 5 * dim["w"] * dim["l"],
-        "R": 0.2 * dim["l"] / dim["w"],
-    }
-    return achieved
-
-
-dim_f, par_f, goal_f = space_map(coarse_model, dim0, fine_model, par0, goal)
+        st.write("Final value of the capacitor.")
+        st.experimental_show(dim_f)
