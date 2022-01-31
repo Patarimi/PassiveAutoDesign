659f4fe284a1db20ba74928e21e6e0cae554fe79
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-01-31 15:20:25 +0100

    Small display improvements

--- apps/example_space_mapping.py
+++ apps/example_space_mapping.py
@@ -12,15 +12,18 @@ import passive_auto_design.components.lumped_element as lp
 class SpaceMap(HydraHeadApp):
     def run(self):
         st.header("Space mapping example : Optimisation of a capacitor parasitic resistor")
+
         st.write("Definition of the goal (or target)")
-        goal = {"C": 800e-12, "R": (0, 1)}
-        st.experimental_show(goal)
+        with st.echo():
+            goal = {"C": 800e-12, "R": (0, 1)}
+
         st.write("List of the parameters use by the component model.")
-        par0 = {"rho/h": 1e-6, "eps/d": 3}
-        st.experimental_show(par0)
+        with st.echo():
+            par0 = {"rho/h": 1e-6, "eps/d": 3}
+
         st.write("List of the dimensions of the component.")
-        dim0 = {"l": 1e-3, "w": 1e-6}
-        st.experimental_show(dim0)
+        with st.echo():
+            dim0 = {"l": 1e-3, "w": 1e-6}
 
         st.write("Definition of the coarse model.")
         with st.echo():
@@ -46,4 +49,4 @@ class SpaceMap(HydraHeadApp):
             dim_f, par_f, goal_f = space_map(coarse_model, dim0, fine_model, par0, goal)
 
         st.write("Final value of the capacitor.")
-        st.experimental_show(dim_f)
+        st.json(dim_f)
