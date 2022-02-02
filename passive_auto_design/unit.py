e7000145052be5c6d33fe0ca31a3aa4bb62139c9
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-02-02 08:44:01 +0100

    Adding some comments

--- passive_auto_design/unit.py
+++ passive_auto_design/unit.py
@@ -1,14 +1,28 @@
+"""
+Some function to format physical dimensions.
+"""
+
 from matplotlib.ticker import EngFormatter
 from numpy import real, imag
 
+"""
+Format frequency to string.
+"""
 Frequency = EngFormatter(unit="Hz")
 
 __res = EngFormatter(unit="")
 
 
 def Impedance(value):
+    """
+    Format complex impedance to string with engineer format.
+    """
     if imag(value) < 0:
         return __res(real(value)) + " - j" + __res(-imag(value)) + r" $\Omega$"
     return __res(real(value)) + " + j" + __res(imag(value)) + r" $\Omega$"
 
+
+"""
+Format number to string in engineer format without unit.
+"""
 SI = EngFormatter(unit="")
