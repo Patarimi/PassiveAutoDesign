9524d943c3bda23035358c1a6e6553edf96826a9
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-03-03 16:55:05 +0100

    refac transformer.py using lumped_element class change lumped_element class to enable access to sub-part

--- passive_auto_design/components/transformer.py
+++ passive_auto_design/components/transformer.py
@@ -9,7 +9,7 @@ from passive_auto_design.special import u0
 import passive_auto_design.components.lumped_element as lmp
 
 
-class Transformer:
+class Transformer(lmp.LumpedElement):
     """
     Create a transformer object with the specified geometry _primary & _secondary
     (which are dict defined as :
@@ -17,81 +17,26 @@ class Transformer:
     and calculate the associated electrical model
     """
 
-    def __init__(self, primary, secondary, freq=1e9, model_map_file=None):
-        self.prim = primary
-        self.second = secondary
-        if isinstance(freq, rf.Frequency):
-            self.freq = freq
-        elif isinstance(freq, float):
-            self.freq = rf.Frequency(freq, freq, 1, unit="Hz")
-        else:
-            self.freq = rf.Frequency.from_f(freq, unit="Hz")
-        if model_map_file is None:
-            model_map_file = "tests/default.map"
-        with open(model_map_file, "r") as file:
-            self.model_map = yaml.full_load(file)
-        self.model = {}
-        self.set_primary(primary)
-        self.set_secondary(secondary)
+    def __init__(self, primary, secondary, rho=0., eps_r=0., h_mut=0., h_gnd=0.):
+        lmp.LumpedElement.__init__(self)
+        self.par = {}
+        self.par["rho"] = rho
+        self.par["eps_r"] = eps_r
+        self.par["h_mut"] = h_mut
+        self.par["h_gnd"] = h_gnd
+        self.par["lp"] = primary
+        self.par["ls"] = secondary
+        self.par["rp"] = self.r_geo(True)
+        self.par["rs"] = self.r_geo(False)
+        self.par["cm"] = self.cc_geo(True)
+        self.par["cg"] = self.cc_geo(False)
+        self.par["k"] = self.k_geo()
+        self.ref = "k"
 
-    def set_primary(self, _primary):
-        """
-        modify the top inductor and refresh the related model parameters
-        """
-        self.prim = _primary
-        self.model.update(
-            {
-                "lp": self.l_geo(True),
-                "rp": self.r_geo(True),
-                "cm": self.cc_geo(True),
-                "cg": self.cc_geo(False),
-                "k": self.k_geo(),
-            }
-        )
-        try:
-            self.circuit = self.__make_circuit()
-        except KeyError:
-            pass
-
-    def set_secondary(self, _secondary):
-        """
-        modify the bottom inductor and refresh the related model parameters
-        """
-        self.second = _secondary
-        self.model.update(
-            {
-                "ls": self.l_geo(False),
-                "rs": self.r_geo(False),
-                "cg": self.cc_geo(False),
-                "cm": self.cc_geo(True),
-                "k": self.k_geo(),
-            }
-        )
-        try:
-            self.circuit = self.__make_circuit()
-        except KeyError:
-            pass
-
-    def l_geo(self, _of_primary=True):
-        """
-        return the value of the distributed inductance of the described transformer
-        if _of_primary, return the value of the top inductor
-        else, return the value of the bottom inductor
-        """
-        k_1 = float(self.model_map["mu_r"])  # constante1 empirique pour inductance
-        k_2 = float(self.model_map["dens"])  # constante2 empirique pour inductance
-        if _of_primary:
-            geo = self.prim
-        else:
-            geo = self.second
-        outer_diam = (
-            geo["di"]
-            + 2 * geo["n_turn"] * geo["width"]
-            + 2 * (geo["n_turn"] - 1) * geo["gap"]
-        )
-        rho = (geo["di"] + outer_diam) / 2
-        density = (outer_diam - geo["di"]) / (outer_diam + geo["di"])
-        return k_1 * u0 * geo["n_turn"] ** 2 * rho / (1 + k_2 * density)
+    def calc_ref_value(self):
+        for subpart in ("ls", "lp", "rp", "rs", "cm", "cg"):
+            self.par[subpart].calc_ref_value()
+        return self.k_geo()
 
     def cc_geo(self, _mutual=True):
         """
@@ -99,96 +44,50 @@ class Transformer:
         if _mutual, return the capacitance between primary and secondary
         else, return the capacitance to the ground plane
         """
+        l1 = self.par["lp"].par
+        l2 = self.par["ls"].par
         if _mutual:
-            dist = float(self.model_map["d_m"])
-            d_i = np.max((self.prim["di"], self.second["di"]))
-            d_o = np.min((self.prim["di"] + self.prim["n_turn"]*self.prim["width"],
-                         self.second["di"] + self.second["n_turn"]*self.second["width"]))
+            dist = float(self.par["h_mut"])
+            d_i = np.max([l2["d_i"], l1["d_i"]])
+            d_o = np.min([l1["d_i"] + l1["n_turn"] * l1["width"], l2["d_i"] + l2["n_turn"] * l2["width"]])
         else:
-            dist = float(self.model_map["d_g"])
-            d_i = self.second["di"]
-            d_o = self.second["di"] + self.second["n_turn"] * self.second["width"]
-        eps_r = float(self.model_map["eps_r"])
+            dist = float(self.par["h_gnd"])
+            d_i = np.min([l2["d_i"], l1["d_i"]])
+            d_o = np.max([l1["d_i"] + l1["n_turn"] * l1["width"], l2["d_i"] + l2["n_turn"] * l2["width"]])
+        n_t = self.par["lp"].par["n_turn"]
+        eps_r = float(self.par["eps_r"])
         area = 4 * (d_o ** 2 - d_i ** 2) * (1 + 2 * np.sqrt(2))
         cap = lmp.Capacitor(area, dist, eps_r)
-        return cap.par["cap"]
+        return cap
 
     def r_geo(self, _of_primary=True):
         """
         return the value of the resistance of the described transformer
         """
         if _of_primary:
-            geo = self.prim
+            geo = self.par["lp"].par
         else:
-            geo = self.second
-        rho = self.model_map["rho"]
+            geo = self.par["ls"].par
+        rho = self.par["rho"]
         n_t = geo["n_turn"]
         l_tot = (
             8
             * np.tan(np.pi / 8)
             * n_t
-            * (geo["di"] + geo["width"] + (n_t - 1) * (geo["width"] + geo["gap"]))
+            * (geo["d_i"] + geo["width"] + (n_t - 1) * (geo["width"] + geo["gap"]))
         )
-        r_dc = rho * l_tot / geo["width"]
-        return np.maximum(r_dc, 0)
+        r_dc = lmp.Resistor(geo["width"], l_tot, rho)
+        return np.maximum(r_dc.par["res"], 0)
 
     def k_geo(self):
         """
         return the value of the coupling between the two inductors.
 
         """
-        return self.model_map["cpl_eq"]
-
-    def mutual_geo(self, z_0=50):
-        """
-        Create a transformer with a primary of l_1, and secondary of l_2
-        and a coupling factor of k_mut
-        """
-        l_1 = self.model["lp"]
-        l_2 = self.model["ls"]
-        r_1 = self.model["rp"]
-        r_2 = self.model["rs"]
-        k_mut = self.model["k"]
-        for f_t in self.freq.f:
-            w_t = float(2 * np.pi * f_t)
-            y_1 = 1 / (r_1 + 1j * w_t * l_1 * (1 - k_mut ** 2))
-            y_2 = 1 / (r_2 + 1j * w_t * l_2 * (1 - k_mut ** 2))
-            y_m = k_mut / (1e-30 + 1j * w_t * np.sqrt(l_1 * l_2) * (1 - k_mut ** 2))
-            y_param = np.array(
-                [
-                    [
-                        [y_1, -y_m, y_m, -y_1],
-                        [-y_m, y_2, -y_2, y_m],
-                        [y_m, -y_2, y_2, -y_m],
-                        [-y_1, y_m, -y_m, y_1],
-                    ]
-                ]
-            )
-            try:
-                y_params = np.vstack((y_params, y_param))
-            except NameError:  # Only needed for first iteration.
-                y_params = np.copy(y_param)
-        ntwk = rf.Network(
-            frequency=self.freq, s=rf.y2s(y_params), z0=z_0, name="coupled inductors"
-        )
-        return ntwk
-
-    def __make_circuit(self):
-        media = rf.media.DefinedGammaZ0(frequency=self.freq, Z0=50)
-        transfo_ideal = self.mutual_geo()
-        cap_g, ports = [], []
-        for i in range(4):
-            cap_g.append(media.capacitor(self.model["cg"], name=f"cg{i}"))
-            ports.append(rf.Circuit.Port(self.freq, f"port{i}"))
-        cap_m1 = media.capacitor(self.model["cm"], name="cm1")
-        cap_m2 = media.capacitor(self.model["cm"], name="cm2")
-
-        connections = []
-        for i in range(4):
-            connections.append([(ports[i], 0), (transfo_ideal, i), (cap_g[i], 0)])
-        connections.append([(cap_m1, 0), (transfo_ideal, 0)])
-        connections.append([(cap_m1, 1), (transfo_ideal, 1)])
-        connections.append([(cap_m2, 0), (transfo_ideal, 3)])
-        connections.append([(cap_m2, 1), (transfo_ideal, 2)])
-        cir = rf.Circuit(connections)
-        return cir
+        l1 = self.par["lp"].par
+        l2 = self.par["ls"].par
+        c1 = l1["width"]*l1["n_turn"] + l1["gap"]*(l1["n_turn"]-1)
+        d1 = l1["d_i"]
+        c2 = l2["width"]*l2["n_turn"] + l2["gap"]*(l2["n_turn"]-1)
+        d2 = l2["d_i"]
+        return 0.99*(np.min([c1+d1, c2+d2])-np.max([d1, d2]))/(np.max([c1+d1, c2+d2])-np.min([d1, d2]))
