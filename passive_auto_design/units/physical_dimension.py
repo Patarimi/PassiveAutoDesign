791be94a7bb49deed4e3a541d3ac060dab5b4685
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-07-22 17:18:05 +0200

    revert

--- passive_auto_design/units/physical_dimension.py
+++ passive_auto_design/units/physical_dimension.py
@@ -20,13 +20,10 @@ class NDArray(np.ndarray):
             isinstance(v, float)
             or isinstance(v, np.float32)
             or isinstance(v, np.float64)
-            or isinstance(v, np.float128)
             or isinstance(v, int)
             or isinstance(v, np.int32)
             or isinstance(v, np.int64)
-            or isinstance(v, np.int128)
             or isinstance(v, complex)
-            or isinstance(v, np.complex32)
             or isinstance(v, np.complex64)
             or isinstance(v, np.complex128)
         ):
