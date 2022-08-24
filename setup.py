3edbdb99bc786afcf8f1cec52b117df976b56ce6
Patarimi <38954040+Patarimi@users.noreply.github.com>
2022-08-24 10:59:16 +0200

    bump version to 0.1.12 update dependencies update documentation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
 
 setuptools.setup(
     name="passive-auto-design",
-    version="0.1.11",
+    version="0.1.12",
     author="Patarimi",
     author_email="mpqqch@gmail.com",
     description="Tools for fast prototyping of radio-frequence passive components",
